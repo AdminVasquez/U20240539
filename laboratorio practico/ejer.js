0670c95afb72d462d5604a5a79a0fe2b357ffe43
AdminVasquez <jvnvsaz@gmail.com>
2024-08-16 11:35:07 -0600

    Create ejer.js

---
+++ laboratorio practico/ejer.js
@@ -0,0 +1,11 @@
+function numero(numer) {
+    return numer % 2 === 0;
+}
+
+// Ejemplos de uso:
+console.log ("El primer numero es: 4")
+console.log("El segundo numero es: 7 ")
+console.log("Se determinara con TRUE si el numero es par, si es impar se determinara con FALSE")
+
+console.log( "El numero 4 es ", numero(4)); // true
+console.log( "El numero 7 es ", numero(7)); // false
