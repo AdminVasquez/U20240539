51bd040d61767958a8594bb3809945fadf350cac
AdminVasquez <jvnvsaz@gmail.com>
2024-09-06 11:18:55 -0600

    Add files via upload

---
+++ laboratorio practico/practica3/minu.js
@@ -0,0 +1,7 @@
+//Crear un objeto Date con la fecha y la hora actuales
+const fecha_actual = new Date();
+
+//Obtener los minutos actuales
+const minutos = fecha_actual.getMinutes();
+
+console.log("Minutos actuales:", minutos);
---
+++ laboratorio practico/practica3/sec.js
@@ -0,0 +1,7 @@
+//Crear un objeto Date con la fecha y hora actual
+const fecha_actual = Date();
+
+//Obtener los segundos actuales
+const segundos = fecha_actual.getSeconds();
+
+console.log("Segundos actuales:", segundos);
